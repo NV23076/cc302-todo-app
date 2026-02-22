715cb74cfaa20769ab9e9348005742703e22e955
Saud Thani <nv23076@ncst.edu.bh>
2026-02-22 08:48:57 +0000

    nig

---
+++ flask-todo-crud-docker/tests/test_app.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_app_import():
+    from app import create_app
+    app = create_app()
+    assert app is not None
+
+
+def test_app_responds():
+    from app import create_app
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.test_client() as c:
+        rv = c.get('/')
+    assert rv.status_code in [200, 302]
